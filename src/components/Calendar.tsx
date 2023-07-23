0e80906db1649fadd2cdf0a44f1172239077d1c1
nearworld <artemismars2@outlook.com>
2023-07-23 20:26:16 +0900

    refactor: move currentDate state to Calendar

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,53 +1,65 @@
-import { useEffect, useMemo, useState } from "react";
+import { FC, useEffect, useMemo, useState } from "react";
 
 import { Row } from "./Row";
 import { ClickedDate, monthConfig } from "../date.config";
 
-export const ROW_HEIGHT = 60;
-
-const Body = () => {
-  const [clickedDate, setClickedDate] = useState<ClickedDate>();
-
-  const { days, rows } = useMemo(() => monthConfig(), []);
-
-  return (
-    <div
-      style={{
-        display: "grid",
-        marginTop: "10px",
-        backgroundColor: "white",
-        width: `${7 * ROW_HEIGHT}px`,
-        transition: "0.5s, height ease-in-out 0.5s",
-        height: clickedDate?.nth
-          ? `${2 * ROW_HEIGHT}px`
-          : `${7 * ROW_HEIGHT}px`,
-      }}
-    >
-      <Row
-        nth={0}
-        dates={days}
-        clickedDate={clickedDate}
-        setClickedDate={setClickedDate}
-      />
-      {rows.map((dates, idx) => (
-        <Row
-          key={idx}
-          dates={dates}
-          nth={idx + 1}
-          clickedDate={clickedDate}
-          setClickedDate={setClickedDate}
-        />
-      ))}
-    </div>
-  );
+type CurrentDate = {
+  year: number;
+  month: number;
+};
+type TopBarPropsType = {
+  currentDate: CurrentDate;
+  setCurrentDate: (value: CurrentDate) => void;
 };
 
-const TopBar = () => {
-  const [currentDate, setCurrentDate] = useState({
+export const ROW_HEIGHT = 60;
+
+export const Calendar = () => {
+  const [clickedDate, setClickedDate] = useState<ClickedDate>();
+
+  const [currentDate, setCurrentDate] = useState<CurrentDate>({
     year: new Date().getFullYear(),
     month: new Date().getMonth() + 1,
   });
 
+  const { days, rows } = useMemo(() => monthConfig(), []);
+
+  return (
+    <>
+      <TopBar currentDate={currentDate} setCurrentDate={setCurrentDate} />
+      <div
+        style={{
+          display: "grid",
+          marginTop: "10px",
+          backgroundColor: "white",
+          width: `${7 * ROW_HEIGHT}px`,
+          transition: "0.5s, height ease-in-out 0.5s",
+          height: clickedDate?.nth
+            ? `${2 * ROW_HEIGHT}px`
+            : `${7 * ROW_HEIGHT}px`,
+        }}
+      >
+        <Row
+          nth={0}
+          dates={days}
+          clickedDate={clickedDate}
+          setClickedDate={setClickedDate}
+        />
+        {rows.map((dates, idx) => (
+          <Row
+            key={idx}
+            dates={dates}
+            nth={idx + 1}
+            clickedDate={clickedDate}
+            setClickedDate={setClickedDate}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
+
+const TopBar: FC<TopBarPropsType> = ({ currentDate, setCurrentDate }) => {
   useEffect(() => {
     console.log(currentDate);
   }, [currentDate]);
@@ -90,15 +102,19 @@ const TopBar = () => {
       <button style={buttonStyle} onClick={() => changeMonth("prev")}>
         {"<"}
       </button>
-      <div></div>
+      <div
+        style={{
+          display: "grid",
+          placeItems: "center",
+        }}
+      >
+        <span>
+          {currentDate.year} / {currentDate.month}
+        </span>
+      </div>
       <button style={buttonStyle} onClick={() => changeMonth("next")}>
         {">"}
       </button>
     </header>
   );
-};
-
-export const Calendar = {
-  Body,
-  TopBar,
 };
